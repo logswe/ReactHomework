24487c8bbeca291f29c2f54ff47c6ccde0eb0e9e
logswe <79446329+logswe@users.noreply.github.com>
2021-05-27 01:43:37 +0300

    test commit

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePageContainer from './pages/HomePage/containers/HomePageContainer';
+import './styles/index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HomePageContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ app/src/pages/HomePage/containers/HomePageContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import UserCard from "../components/UserCard"
+
+class HomePageContainer extends Component {
+    render() {
+        return <UserCard />
+    }
+}
+
+export default HomePageContainer;
